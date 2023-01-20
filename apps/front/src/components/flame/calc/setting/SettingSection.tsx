581af754357b88692f8df8e0c9600c0c12dfd01c
vetan2 <vetan2@naver.com>
2023-01-20 15:44:38 +0900

    Restore WeaponTypeSelect

--- apps/front/src/components/flame/calc/setting/SettingSection.tsx
+++ apps/front/src/components/flame/calc/setting/SettingSection.tsx
@@ -6,6 +6,7 @@ import { EquipLevelInput } from "~/components/flame/calc/setting/EquipLevelInput
 import { EquipTypeSelect } from "~/components/flame/calc/setting/EquipTypeSelect";
 import { StatSettingButton } from "~/components/flame/calc/setting/StatSettingButton";
 import { WeaponGradeSelect } from "~/components/flame/calc/setting/WeaponGradeSelect";
+import { WeaponTypeSelect } from "~/components/flame/calc/setting/WeaponTypeSelect";
 
 export const SettingSection = () => {
   return (
@@ -13,10 +14,11 @@ export const SettingSection = () => {
       <SectionTitle>설정</SectionTitle>
       <Box sx={styles.inputContainer}>
         <EquipTypeSelect />
-        <BossDropCheckbox />
+        <WeaponTypeSelect />
         <EquipLevelInput />
         <WeaponGradeSelect />
         <AimStatInput />
+        <BossDropCheckbox />
       </Box>
       <StatSettingButton />
     </SectionBox>
@@ -29,9 +31,10 @@ const styles = {
     gridTemplateColumns: "repeat(2, 1fr)",
     gap: 16,
 
-    "& > *:nth-child(2n+1):nth-last-of-type(1)": {
-      gridColumn: "span 2",
-    },
+    // 마지막 item이 2칸을 차지해야 하면 주석 해제
+    // "& > *:nth-child(2n+1):nth-last-of-type(1)": {
+    //   gridColumn: "span 2",
+    // },
   }),
   weaponTypeSelect: sx({
     gridColumn: "2 / 3",
