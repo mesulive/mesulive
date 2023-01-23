d090101b674e224f013de709535f1a55d65d5c9f
vetan2 <vetan2@naver.com>
2023-01-23 12:01:43 +0900

    Remove log

--- apps/front/src/lib/bonus-stat/calcProbWorker.ts
+++ apps/front/src/lib/bonus-stat/calcProbWorker.ts
@@ -3,7 +3,6 @@ import { getMethodProbMap, GetProbPerMethod } from "~/lib/bonus-stat/util";
 addEventListener(
   "message",
   (event: MessageEvent<Parameters<GetProbPerMethod>[0]>) => {
-    console.log("start");
     postMessage(getMethodProbMap(event.data));
   }
 );
